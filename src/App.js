23ebc7425e7c809a8ea66a3f3006d3c1b7463329
I-fenoy <fenoyilyan@gmail.com>
2025-04-22 14:20:47 +0200

    Création du projet, mise en place des fichiers + header bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav class="navbar navbar-expand-lg bg-dark nav-header">
+          <div class="container-fluid">
+            <h1 class="navbar-brand text-light" href="#">JOHN DOE</h1>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+              aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse div-nav-link" id="navbarTogglerDemo02">
+
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link active text-light" aria-current="page" href="#">services</a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link active text-light" aria-current="page" href="#">portfolio</a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link active text-light" aria-current="page" href="#">Contact</a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link active text-light" aria-current="page" href="#">mentions légales</a>
+                </li>
+                
+              </ul>
+
+            </div>
+          </div>
+        </nav>
       </header>
+
+      <footer>
+        
+      </footer>
     </div>
   );
 }
