19ca75fa3711909c0376ee39c9ecee96719740d4
I-fenoy <fenoyilyan@gmail.com>
2025-06-02 15:23:52 +0200

    Effet graphiques cards

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,13 +9,13 @@ const Footer = () => {
             <div className='footer-div1 footer-block'>
                 <address>
                     <strong className='footer-link d-block'>John Doe</strong>
-                    <a href='https://www.google.fr/maps/place/40+Rue+Laure+Diebold,+69009+Lyon/@45.778662,4.7989789,17z/data=!3m1!4b1!4m6!3m5!1s0x47f4eb65edac5b3f:0xe01c47049cb2e2b9!8m2!3d45.778662!4d4.7989789!16s%2Fg%2F11c24790th?entry=ttu&g_ep=EgoyMDI1MDQyOC4wIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D' target='_blank' className='footer-link d-block'>40 rue Laure Diebold<br /> 69009 Lyon, France</a>
-                    <a href='tel:1020304050' target='_blank' className='footer-link d-block'>10 20 30 40 50</a>
-                    <a href='mailto:john.doe@gmaiil.com' target='_blank' className='footer-link d-block'>john.doe@gmail.com</a>
+                    <a href='https://www.google.fr/maps/place/40+Rue+Laure+Diebold,+69009+Lyon/@45.778662,4.7989789,17z/data=!3m1!4b1!4m6!3m5!1s0x47f4eb65edac5b3f:0xe01c47049cb2e2b9!8m2!3d45.778662!4d4.7989789!16s%2Fg%2F11c24790th?entry=ttu&g_ep=EgoyMDI1MDQyOC4wIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D' target='_blank' className='footer-link d-block active'>40 rue Laure Diebold<br /> 69009 Lyon, France</a>
+                    <a href='tel:1020304050' target='_blank' className='footer-link d-block active'>10 20 30 40 50</a>
+                    <a href='mailto:john.doe@gmaiil.com' target='_blank' className='footer-link d-block active'>john.doe@gmail.com</a>
                     <div className='footer-logo'>
-                        <a className='social-img' target="_blank"><img src={github} ></img></a>
-                        <a className='social-img' target="_blank"><img src={linkedin} ></img></a>
-                        <a className='social-img' target="_blank"><img src={twitter} ></img></a>
+                        <a className='social-img active' target="_blank"><img src={github} ></img></a>
+                        <a className='social-img active' target="_blank"><img src={linkedin} ></img></a>
+                        <a className='social-img active' target="_blank"><img src={twitter} ></img></a>
                     </div>
                 </address>
             </div>
